0f562f8e95cb37410fe482b687619728ba7fdaf7
Toru Shimogaisho <gaishmo@gmail.com>
2013-11-15 14:30:32 +0900

    Add process of closing redis connection

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ var port = process.env.PORT || 5000;
 var config = require('config');
 var redis_config = config.redis;
 
-http.createServer(function(req, res){
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+var counter = 0;
 
+http.createServer(function(req, res){
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   var redisClient = redis.createClient(redis_config.port, redis_config.host);
   if( redis_config.password !== '' ){
     redisClient.auth( redis_config.password );
   }
   redisClient.set('key1', 1,  function(){
     redisClient.get('key1', function(err, val){
-      res.end(val);
+      res.end(++counter);
+      //close connection
+      setTimeout(function(){
+        redisClient.quit();
+      }, 1000 * 60 * 10);
     });
   });
 }).listen(port);
