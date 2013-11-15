9889a1bfa03df41228c4da6a19fc761305cb80e4
Toru Shimogaisho <gaishmo@gmail.com>
2013-11-15 13:21:06 +0900

    Change content-type

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var redis = require('hiredis');
 var port = process.env.PORT || 5000;
 
 http.createServer(function(req, res){
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('hello');
 }).listen(port);
