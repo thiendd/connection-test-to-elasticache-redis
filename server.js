64edb6f5af169922d777a385a42bb34ff4e8b097
Toru Shimogaisho <gaishmo@gmail.com>
2013-11-15 14:35:56 +0900

    Modify redis connection counter

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ var redis_config = config.redis;
 var counter = 0;
 
 http.createServer(function(req, res){
+  console.log(req.url);
+  if(req.url === '/favicon.ico'){
+    res.end(''); return;
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   var redisClient = redis.createClient(redis_config.port, redis_config.host);
   if( redis_config.password !== '' ){
     redisClient.auth( redis_config.password );
   }
+  counter++;
   redisClient.set('key1', 1,  function(){
     redisClient.get('key1', function(err, val){
-      res.end(++counter);
+      res.end(counter.toString());
       //close connection
       setTimeout(function(){
         redisClient.quit();
