ad2eef4a26a54b2b7e61c421347f377bc64bf82e
Toru Shimogaisho <gaishmo@gmail.com>
2013-11-15 13:08:59 +0900

    Change port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var redis = require('hiredis');
+var port = process.env.PORT || 5000;
 
 http.createServer(function(req, res){
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('hello');
-}).listen(8080);
+}).listen(port);
